c58de719f5b15c4b922ed5ee3b72a66cf5b00fb4
mengdi.xu <mengdi.xu@shopee.com>
2026-05-24 07:42:52 +0800

    比较通用的问题

--- script.js
+++ script.js
@@ -1,43 +1,37 @@
-// 科幻主题问题列表
 const questions = [
-   
-    "Describe the last small thing that made you laugh for no reason.（最近一件让你无缘无故笑出来的小事是什么？）",
-    
-    "When you were a child, what did you do just for fun — but adults thought it was useless?（小时候你做过哪些只是为了好玩，但大人觉得没用的事？）",
-    
-    "What is something you enjoy, but you almost never tell other people?（有没有一件你其实很喜欢，但几乎不跟别人说的事？）",
-    
-    "If you had one free afternoon and no phone, no computer, what would you do just for fun?（如果你有一个完全空闲的下午，而且不能用手机和电脑，你会做什么来打发时间？）",
-    
-    "What do you usually do for fun when you feel tired after work or study?（下班或下课很累的时候，你一般怎么放松、找点乐子？）",
-    
-    "If money and other people’s opinions didn’t matter, what would you like to try just for fun?（如果不用担心钱，也不用在意别人看法，你最想尝试什么纯为了好玩的事？）",
-    
-    "Do you think ‘just for fun’ is important for adults? Why or why not?（你觉得“只是为了开心”对成年人重要吗？为什么？）",
-    
-    "What is a very cheap or free thing that brings you a lot of fun?（有没有一种很便宜，甚至免费的快乐来源？）",
-    
-    "What was your favorite way to have fun when you were a child or a student? Is it different now?（你小时候或学生时代最喜欢的娱乐方式是什么？现在有变化吗？）",
-    
-    "Have you ever tried something just for fun and then accidentally became quite good at it?（你有没有因为好玩开始做一件事，结果后来还挺擅长的？）",
-    
-    "What do you usually watch, read, or listen to just for fun — not for learning or self-improvement?（你平时纯为了娱乐，会看什么、读什么或听什么？不是为了学习那种。）",
-    
-    "What is one silly habit you have that makes you feel happy?（你有没有一个有点傻的小习惯，但会让你很开心？）",
-    
-    "When was the last time you did something only because you wanted to, not because you had to?（你上一次做一件事只是因为想做，而不是因为必须，是在什么时候？）",
-    
-    "If your job suddenly disappeared for one month, how would you spend that month just for fun?（如果你的工作突然消失一个月，你会怎么用这一个月来好好玩？）",
-    
-    "What is the most interesting thing you did in the last month?（你最近一个月做过的最有趣的一件事是什么？）",
-    
-    "If you could have a monthly ‘fun budget’ that could only be used to make you happy, what would you spend it on?（如果你每个月有一笔只能用来让自己开心的“快乐预算”，你会花在什么上？）",
-    
-    "What’s a simple and free activity that always cheers you up, like blowing bubbles, skipping, or singing off-key?（有没有一种简单又免费的事，总能让你开心，比如吹泡泡、跳绳、跑调唱歌？）",
-    
-    "If you could invent a new holiday just for fun, what would it celebrate and how would people celebrate it?（如果你能发明一个纯为了好玩的节日，它会庆祝什么？大家会怎么过？）",
-    
-    "What’s a funny or weird talent you have that most people don’t know about?（你有什么好笑或奇怪的小技能，是大多数人不知道的？）"    
+  "If you had to describe yourself in three words, which three would you choose? Why?（如果让你用三个词形容自己，你会选哪三个？为什么？）",
+
+"What do you think is the most unique thing about you?（你觉得自己身上最“与众不同”的地方是什么？）",
+
+"Has there ever been an experience that suddenly changed one of your ideas or habits?（有没有一次经历让你突然改变了某个想法或者习惯？）",
+
+"What do you usually do to relax when you feel stressed?（你平时压力大的时候，会用什么方式让自己放松？）",
+
+"If you could recommend a place, movie, book, or game that you really love, what would it be?（如果让你推荐一个自己特别喜欢的地方/电影/书/游戏，你会推荐什么？）",
+
+"If you could go back in time and say one thing to your younger self, what would you say?（如果可以回到过去和小时候的自己说一句话，你会说什么？）",
+
+"What does your “ideal day” look like?（你觉得“理想的一天”是什么样子的？）",
+
+"What was the last thing that made you laugh really hard?（最近一次让你大笑的事情是什么？）",
+
+"If you suddenly had one month of vacation, how would you spend it?（如果突然多出一个月假期，你会怎么安排？）",
+
+"What is the one thing you most want to improve about yourself right now?（你现在最想提升自己的哪一方面？）",
+
+"Is there a sentence or quote that has always influenced you?（有没有一句话一直影响着你？）",
+
+"What is the most important lesson you’ve learned while growing up?（你觉得成长过程中学到的最重要的一课是什么？）",
+
+"If you suddenly had a superpower, what power would you want the most?（如果你突然有超能力，你最想要什么？）",
+
+"What kind of person do you think you are?（你觉得自己是个什么样的人？）",
+
+"What have you been paying attention to or interested in recently?（你最近在关注或者感兴趣的东西是什么？）",
+
+"If you could share one useful life tip, what would it be?（如果让你分享一个生活小经验，你会分享什么？）",
+
+"What kind of animal do you think you are most like?（你觉得自己最像哪一种动物？）"
 ];
 
 // 游戏状态
